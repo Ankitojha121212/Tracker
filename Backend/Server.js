1d8c2c791bc397248f32ea4a9a72ec967451be35
Ankit Ojha <Ankitojha121212@gmail.com>
2025-02-28 18:02:45 +0530

    mongodb is connected to the project

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost:27017/attandanceTracker",{
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+}).then(()=>{console.log("Database connected successfully")})
+.catch((err)=>{console.log("Database is not connected : ",err)})
 
-
+app.get("/",(req,res)=>{
+        res.send("Connected database via mongoose !");
+})
 
 app.listen(port,()=>{
         console.log(`Server started at ${port}`);
